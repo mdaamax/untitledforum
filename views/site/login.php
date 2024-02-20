0f1090bddef430eec48d69718bfcb3991f40532c
mdaaamax <omgwtfmda@mail.ru>
2024-02-20 15:49:27 +0400

    12

--- views/site/login.php
+++ views/site/login.php
@@ -6,7 +6,7 @@
 
 use yii\bootstrap4\ActiveForm;
 use yii\bootstrap4\Html;
-var_dump(password_hash('admin',PASSWORD_DEFAULT));
+//var_dump(password_hash('admin',PASSWORD_DEFAULT));
 $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
@@ -47,3 +47,55 @@ $this->params['breadcrumbs'][] = $this->title;
         To modify the username/password, please check out the code <code>app\models\User::$users</code>.
     </div>
 </div>
+<!doctype html>
+<html lang="ru" xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html"
+      xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>BEERMAX</title>
+    <!--    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"-->
+    <!--          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">-->
+    <!--    <link rel="stylesheet" href="style.css">-->
+    <!--    <link rel="stylesheet" href="media.css">-->
+    <link rel="stylesheet" href="auto_form.css">
+</head>
+<body>
+
+
+
+
+<div class="wrapper fadeInDown">
+    <div id="formContent">
+
+        <h2 class="active"> Авторизация </h2>
+
+
+
+        <div class="fadeIn first">
+            <img src="https://cdn-icons-png.flaticon.com/512/931/931949.png"  id="icon" alt="User Icon"/>
+        </div>
+
+        <form method="post">
+            <input type="text" name="username"  class="form-control" id="exampleFormControlInput1" placeholder="login">
+            <input type="password" name="password" class="form-control" id="exampleFormControlInput1" placeholder="password">
+
+            <input type="submit" class="fadeIn fourth" value="Войти">
+            <br>
+            <b style="color: crimson">
+                <?=$errorMessage?>
+            </b>
+        </form>
+
+
+        <div id="formFooter">
+            <a class="underlineHover" href="register_form.php">Регистрация</a>
+        </div>
+
+    </div>
+</div>
+
+
+
+</body>
+</html>
